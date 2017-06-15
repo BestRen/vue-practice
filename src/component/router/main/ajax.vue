a98e030ca4e2bc20fd3cb917b433bd7925a8ee3e
Louis <neighborhood0830@gmail.com>
2017-06-15 12:09:05 +0800

    add textarea

--- src/component/router/main/ajax.vue
+++ src/component/router/main/ajax.vue
@@ -3,12 +3,20 @@
         <div v-text="msg"></div>
         <el-button type="primary" v-on:click="callApi">Call Ajax</el-button>
         <div v-text="data"></div>
+        <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea">
+        </el-input>
+        <el-button type="primary" size="large">送出</el-button>
     </div>
 </template>
 <script>
 import { exampleApiCtrl } from './../../../Data/store.js';
 
 export default {
+    data() {
+        return {
+            textarea: ''
+        }
+    },
     mounted() {
         exampleApiCtrl.dispatch('callApi');
     },
